bee1bf0f3d1095db28338c9a0a7baa7c0f9b0bb4
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-29 18:32:34 -0500

    Creating the Routes

---
+++ routes/users.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+//Temporary Users Data
+
+let users = [
+  { id: 1, name: "Shawn Jones", email: "jones.shawn@example.com", age: 36 },
+  { id: 2, name: "Randi Jones", email: "jones.randi@@example.com", age: 37 },
+];
+
+//Route To Get All Users
+
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+//Route To Get User By ID
+
+router.get("/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json(user);
+});
+
+//Route To Create A New User
+
+router.post("/", (req, res) => {
+  const { name, email, age } = req.body;
+  const newUser = { id: users.length + 1, name, email, age };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+//Route To Update A User By ID
+
+router.patch("/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.age = req.body.age || user.age;
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
