5501bc32206d4dd59ac33c3d4c94d825be403f6d
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-29 18:53:02 -0500

    Two assisnments in one day is craaazzzy, that's what happens when I try to play catch up, lesson learned

--- routes/posts.js
+++ routes/posts.js
@@ -19,3 +19,61 @@ let posts = [
 ];
 
 //Route to Get All Posts
+
+router.get("/", (req, res) => {
+  res.json(posts);
+});
+
+//Route to Create A New Post
+
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  };
+
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+// Route to Get Post By ID
+
+router.get("/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send("Post Not Found");
+  }
+});
+
+//Route to Update A Post By ID
+
+router.patch("/:id", (req, res) => {
+  const post = post.find((p) => p.id === parseInt(req.params.id));
+  if (post) {
+    if (req.body.title) {
+      post.title = req.body.title;
+    }
+    if (req.body.content) {
+      post.content = req.body.content;
+    }
+    if (req.body.author) {
+      post.author = req.body.author;
+    }
+    res.json(post);
+  } else {
+    res.status(404).send("Post Not Found");
+  }
+});
+
+//Route to Delete A Post By ID
+
+router.delete("/:id", (req, res) => {
+  posts = posts.filter((p) => p.id !== parseInt(req.params.id));
+  res.status(204).send();
+});
+
+module.exports = router;
